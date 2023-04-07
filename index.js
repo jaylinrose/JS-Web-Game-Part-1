a7521649b6db514860e813bb0d6e89affa574c38
jaylinrose <jaylin.manuel02@gmail.com>
2023-04-06 21:23:35 -0700

    added pine tree and defined newImage ()

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+function newImage () {
+let parameters = document.createElement('img')
+parameters.src = 'assets/green-character.gif'
+parameters.style.position = 'fixed'
+parameters.style.left = '100px'
+parameters.style.bottom = '100px'
+document.body.append(parameters)
+}
+
+
+
+
 let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
